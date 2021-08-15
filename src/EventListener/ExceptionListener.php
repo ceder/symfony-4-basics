a24c15e2acb42ac32bc87e0da5b8f5e61311e987
ceder <ceder16@gmail.com>
2021-08-15 19:55:22 +0200

    Remove the extend and replace GetResponseForExceptionEvent to ExceptionEvent

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -5,14 +5,13 @@ namespace App\EventListener;
 use ApiPlatform\Core\Exception\InvalidArgumentException;
 use ApiPlatform\Core\Exception\ItemNotFoundException;
 use ApiPlatform\Core\Bridge\Symfony\Validator\Exception\ValidationException;
-use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
-use Symfony\Component\HttpKernel\EventListener\ExceptionListener as BaseExceptionListener;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\Validator\ConstraintViolation;
 use Symfony\Component\Validator\ConstraintViolationList;
 
-class ExceptionListener extends BaseExceptionListener
+class ExceptionListener
 {
-    public function onKernelException(GetResponseForExceptionEvent $event)
+    public function onKernelException(ExceptionEvent $event)
     {
         $request = $event->getRequest();
         // Normalize exceptions only for routes managed by API Platform
